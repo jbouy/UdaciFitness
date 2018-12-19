184e058674b882ac513d06e2b01e7d5ae37fc3df
James Bouy <jbouy@turner-industries.com>
2018-12-18 22:42:13 -0600

    Changes for History-AppLoading

--- components/History.js
+++ components/History.js
@@ -14,8 +14,13 @@ import UdaciFitnessCalendar from "udacifitness-calendar";
 import { white } from "../utils/colors";
 import DateHeader from "./DateHeader";
 import MetricCard from "./MetricCard";
+import { AppLoading } from "expo";
 
 class History extends Component {
+  state = {
+    ready: false,
+  };
+
   componentDidMount() {
     const { dispatch } = this.props;
 
@@ -29,7 +34,12 @@ class History extends Component {
             })
           );
         }
-      });
+      })
+      .then(() =>
+        this.setState(() => ({
+          ready: true,
+        }))
+      );
   }
 
   renderItem = ({ today, ...metrics }, formattedDate, key) => (
@@ -60,6 +70,11 @@ class History extends Component {
 
   render() {
     const { entries } = this.props;
+    const { ready } = this.state;
+
+    if (ready === false) {
+      return <AppLoading />;
+    }
 
     return (
       <UdaciFitnessCalendar
